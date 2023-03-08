f3ca586d4c71ef6d504e728ade2e08cba3c3b484
Robson Ferreira <115955245+RobsonFe@users.noreply.github.com>
2023-03-08 10:05:25 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+palavra = "Robson"
+
+for letra in palavra:
+    print(letra)
+
+#A variavel letra pesquisa dentro da variavel palavra cada letra que contem nela através da estrutura do "For", isso facilita a utilização de listas, vetores e matrizes no Python na hora de pecorrer o que possui no stack da variavel. 
